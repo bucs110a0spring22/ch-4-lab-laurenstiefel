593edeff1d5d63bf0a61271f7e0160b03d765e7f
Lauren  H <98533788+laurenstiefel@users.noreply.github.com>
2022-03-07 16:47:28 +0000

    Completed the lab with all 3 curves

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ < The program draws sine, cosine and tangent curves. This program also adjusts the axis to make the curves more visible >
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: < 0 >
 
-Grace days remaining: < # >/5
+Grace days remaining: < 5 >/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ < N/A >
 
 ## REFERENCES:
- < List any outside resources used >
+ < [List any outside resources used ](https://docs.python.org/3.8/library/turtle.html#turtle.setworldcoordinates)>
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ <N/A >
--- main.py
+++ main.py
@@ -6,6 +6,40 @@ import turtle
 # then you do not fully understand functions
 # and should review how they work or ask for help
 
+import math
+import turtle
+
+def setupWindow (wn):
+  wn.setworldcoordinates(-360,-1,360,1)
+def setupAxis (dart):
+  dart.goto(-360,0)
+  dart.goto (360,0)
+  dart.goto(0,0)
+  dart.goto(0,-1)
+  dart.goto (0,1)
+  dart.up()
+def drawSineCurve(dart):
+ for i in range (-360,361):
+    y = math.sin(math.radians(i))
+    dart.goto(i,y)
+    dart.down()
+ dart.up() 
+
+def drawCosineCurve(dart):
+ for i in range (-360,361):
+    y = math.cos(math.radians(i))
+    dart.goto(i,y)
+    dart.down()
+ dart.up()   
+
+def drawTangentCurve(dart):
+ for i in range (-360,361):
+    y = math.tan(math.radians(i))
+    dart.goto(i,y)
+    dart.down()
+
+
+
 
 
 
@@ -19,7 +53,7 @@ def main():
     wn = turtle.Screen()
     wn.tracer(5)
     dart = turtle.Turtle()
-    dart.speed(0)
+    dart.speed(0.1)
     drawSineCurve(dart)
 
     #Part B
